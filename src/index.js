af80fb25335654fa361a0e0d2fbd01ecffeb9a48
avinash <csengineer.avinash@gmail.com>
2019-03-02 17:22:04 +0530

    removed node_modules folder

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.interceptors.request.use(request => {
     console.log(request);
     //EDIT request
-    return request;
+    return request; 
 }, error => {
     console.log(error);
     return Promise.reject(error);
