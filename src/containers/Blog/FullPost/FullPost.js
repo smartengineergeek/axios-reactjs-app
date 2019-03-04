a79fb46012c5afc80f30b2ed18c72922c5c9267a
avinash <csengineer.avinash@gmail.com>
2019-03-04 12:50:14 +0530

    Added <Redirect from="" to="" />

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -9,9 +9,17 @@ class FullPost extends Component {
     }
 
     componentDidMount(){
-        console.log(this.props);
+        // console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate(){
+        this.loadData();
+    }
+
+    loadData(){
         if(this.props.match.params.id){
-            if( !this.state.fullPost || (this.state.fullPost && this.state.fullPost.id !== this.props.match.params.id)){
+            if( !this.state.fullPost || (this.state.fullPost && this.state.fullPost.id !== +this.props.match.params.id)){
                 axios.get('/posts/'+ this.props.match.params.id)
                 .then(response => {
                     this.setState({fullPost: response.data});
@@ -29,7 +37,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign: 'center'}}>Loading...</p>;
         }
         if(this.state.fullPost) {
